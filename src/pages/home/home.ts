2130e3afc468836336e1ac6a24f87c9ccee0c04c
leooSousa <leomar123456@outlook.com>
2019-03-18 13:44:22 -0300

    primeiro commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { NavController, List } from 'ionic-angular';
+import { Contato } from '../../models/Contato';
+import { HttpClient } from '@angular/common/http';
+import { Tempo } from '../../models/Tempo';
+import { hostElement } from '@angular/core/src/render3/instructions';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  rootPage = HomePage;
+
+  // public contatos: Contato[];
+  public tempo: Tempo[]; 
+  
+  constructor(public navCtrl: NavController, private _http: HttpClient) {
+    
+    this._http.get<Tempo[]>('http://tempo-oeiras.herokuapp.com/api/')
+        .subscribe(
+          (tempo) => {
+            console.log(tempo);
+            this.tempo = tempo
+          }
+        );
+        console.log(this.tempo);
+  }
+}
